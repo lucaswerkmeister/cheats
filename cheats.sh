685c5bccd0a4d9052274a6944dc47579595f97fc
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-10-27 13:17:28 +0100

    Fix cheats.sh
    There was a strange bug. I’m not sure what happened, but this fixes it.

--- cheats.sh
+++ cheats.sh
@@ -30,9 +30,9 @@ function cheats {
 function __run_cheat {
     local file="$*";
     head -n 2 -- "$file"; # print first and second line: description and command
-    local command=$(sed -n '2p' < "$file"); # read second line: command
-    for line in "$(tail -n +3 "$file")"; do # skip the first two lines
-        [[ "$line" = "" ]] && continue;
+    local command=$(sed -n '2p' -- "$file"); # read second line: command
+    for line in "$(tail -n +3 -- "$file")"; do # skip the first two lines
+        [[ "$line" = [[:space:]] ]] && continue;
         local prompt="$(echo "$line" | sed 's/[^:]*:\(.*\)/\1/')";
         local name=$(echo "$line" | sed 's/\([^:]*\):.*/\1/');
         read -p "$prompt$PS2";
