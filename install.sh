8024f67cb198792724a6b99a1ef656952b4a8051
Lucas Werkmeister <mail@lucaswerkmeister.de>
2013-11-08 23:14:21 +0100

    Untabify
    According to grep, that was the last tab in the whole repository. Let’s
    hope it stays that way...

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 if [ "$SHELL" != "/bin/bash" ]; then
     read -p "Your shell does not appear to be `bash`; are you sure you want to install 'cheats'? [y/N]$PS2";
     if [ "$REPLY" != "y" -a "$REPLY" != "Y" ]; then
-	exit;
+        exit;
     fi
 fi
 mkdir -p ~/bin
